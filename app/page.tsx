e6db8be9a72619981c9bb7fde16ba19275e4455e
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-01-26 19:57:16 -0700

    Adds ProjectOverview to LandingPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Blurb from "./components/landing/Blurb";
+import ProjectOverview from "./components/landing/ProjectOverview";
 
 export default function LandingPage() {
   return (
     <div className="LandingPage grid gap-12.5">
       <Blurb />
-      {/* 
       <ProjectOverview />
+      {/* 
+      
       <Contact /> */}
     </div>
   );
