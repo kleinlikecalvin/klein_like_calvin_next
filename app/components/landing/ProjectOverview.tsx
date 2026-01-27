5d69fb60d26ea7c488087b77c2491e5ed3bb14c6
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-01-26 19:56:52 -0700

    Adds styling to ProjectOverview

--- app/components/landing/ProjectOverview.tsx
+++ app/components/landing/ProjectOverview.tsx
@@ -1,17 +1,16 @@
-import "../../styles/components/landing/_ProjectOverview.scss";
-import { projectsData } from "../../data/projects/projectsData.ts";
-import ProjectCard from "../projects/ProjectOverviewCard.tsx";
+import { projectsData } from "../../data/projects/projectsData";
+import ProjectOverviewCard from "../projects/ProjectOverviewCard";
 
 export default function ProjectOverview() {
   return (
-    <section className="ProjectOverview">
-      <div className="cards-container">
-        {projectsData.slice(0, 2).map((project) => {
-          return <ProjectCard project={project} />;
+    <section className="ProjectOverview bg-accent-two rounded-[38px] p-10">
+      <div className="cards-container grid-cols-1 lg:grid-cols-2 gap-10 grid">
+        {projectsData.slice(0, 2).map((project, i) => {
+          return <ProjectOverviewCard project={project} key={i} />;
         })}
       </div>
       {projectsData.length > 2 && (
-        <div className="button-container">
+        <div className="button-container mt-10 text-lg font-medium text-center hover:scale-[1.05]">
           <a href="/all-projects">more projects</a>
         </div>
       )}
