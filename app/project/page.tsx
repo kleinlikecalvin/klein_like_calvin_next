00792f30d6223ba9c4b5a55d2bd993d3271f0fa8
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-03-30 18:36:19 -0700

    Adds some styling to project display page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -8,32 +8,31 @@ export default function Project() {
   const id = searchParams.get("id") || "";
   const project = projectsData.find((project) => project.id === id);
 
+  // TODO: Not found UI
   if (!project) return;
 
   return (
-    <main className="Project">
-      {/*
-year – role 
-title
-preview.Blurb
-Image 0
-githuburl
-achievements
-skills
-the rest of the imagery
-resources
-callouts
-*/}
-      <p className="year-role">
-        <span>{project.year}</span> | <span>{project.role}</span>
+    <main className="Project flex flex-col gap-10">
+      <section className="header flex justify-between">
+        <div className="details">
+          <h1 className="title text-4xl">{project.title}</h1>
+          <p className="year-role">
+            <span>{project.year}</span> | <span>{project.role}</span>
+          </p>
+        </div>
+        <a
+          href={project.page.livePage.url}
+          className="live-page w-max h-max scale"
+        >
+          {project.page.livePage.displayText}
+        </a>
+      </section>
+      <p className="blurb">
+        <span className="special">TLDR: </span>
+        {project.preview.blurb}
       </p>
-      <h1 className="title">{project.title}</h1>
-      <p className="blurb">{project.preview.blurb}</p>
       {project.images && (
-        <img src={project.images[0].src} alt={project.images?.[0].alt} />
-      )}
-      {project.page.gitHubRepoUrl && (
-        <a href={project.page.gitHubRepoUrl} className="git-hub"></a>
+        <img src={project.images[0].src} alt={project.images[0].alt} />
       )}
       <ul className="achievements">
         {project.page.achievements.map((achievement, i) => (
@@ -45,19 +44,31 @@ callouts
           <li key={i}>{skill}</li>
         ))}
       </ul>
-      {/* carousel w/ CSS */}
-      <div className="resources">
-        {project.resources?.map((resource, i) => (
-          <p key={i}>
-            <a href={resource.url}>{resource.displayValue}</a>
-          </p>
-        ))}
-      </div>
-      <ul className="callouts">
-        {project.page.callouts?.map((callout, i) => (
-          <li key={i}>{callout}</li>
-        ))}
-      </ul>
+      {project.images && project.images.length > 1 && (
+        <ul className="images-carousel">
+          {project.images.map((image) => (
+            <li className="image">
+              <img src={image.src} alt={image.alt} />
+            </li>
+          ))}
+        </ul>
+      )}
+      {!!project.resources && (
+        <div className="resources">
+          {project.resources.map((resource, i) => (
+            <p key={i}>
+              <a href={resource.url}>{resource.displayValue}</a>
+            </p>
+          ))}
+        </div>
+      )}
+      {!!project.page.callouts && (
+        <ul className="callouts">
+          {project.page.callouts.map((callout, i) => (
+            <li key={i}>{callout}</li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
