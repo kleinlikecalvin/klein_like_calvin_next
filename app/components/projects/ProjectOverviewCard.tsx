cb4ab40969a5118890b7d1a0061b7ccf4c8470dd
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-04-06 11:28:14 -0700

    Refactors ProjectOverviewCard

--- app/components/projects/ProjectOverviewCard.tsx
+++ app/components/projects/ProjectOverviewCard.tsx
@@ -1,5 +1,6 @@
 import { Project } from "@/app/types/project_types";
 import Image from "next/image";
+import Skills from "./Skills";
 
 export default function ProjectOverviewCard({
   project,
@@ -8,46 +9,43 @@ export default function ProjectOverviewCard({
   project: Project;
   classes?: string;
 }) {
+  const { id, title, preview, skills, year, role } = project;
+
   return (
     <a
-      href={`/project?id=${project.id}`}
+      href={`/project?id=${id}`}
       className={`ProjectOverviewCard rounded-[20px] p-half bg-background text-foreground scale ${classes}`}
     >
-      {project.preview.thumbnail.src && (
+      <p className="special">{title}</p>
+      {preview.thumbnail.src && (
         <div className="image-container h-50 overflow-hidden border-b-special-one border-b">
           <Image
             className="w-full h-auto m-0 mx-auto"
-            src={project.preview.thumbnail.src}
-            alt={project.preview.thumbnail.alt}
-            height={project.preview.thumbnail.height}
-            width={project.preview.thumbnail.width}
+            src={preview.thumbnail.src}
+            alt={preview.thumbnail.alt}
+            height={preview.thumbnail.height}
+            width={preview.thumbnail.width}
           />
         </div>
       )}
-      <div className="info h-65 pb-half border-b border-b-special-one">
+      <div className="info min-h-65 max-h-max pb-half border-b border-b-special-one">
         <div className="skills">
           <p className="special">skills</p>
-          {project.skills.map((skill) => {
-            return (
-              <span className="skill" key={skill}>
-                {skill}
-              </span>
-            );
-          })}
+          <Skills skills={skills} />
         </div>
         <div className="blurb">
           <p className="special">blurb</p>
-          <p className="blurb">{project.preview.blurb}</p>
+          <p className="blurb">{preview.blurb}</p>
         </div>
       </div>
       <div className="stats h-20 flex gap-5 items-start justify-evenly text-center">
         <div className="year stat">
           <p className="special">year</p>
-          <p className="text-sm">{project.year}</p>
+          <p className="text-sm">{year}</p>
         </div>
         <div className="role stat">
           <p className="special">role</p>
-          <p>{project.role}</p>
+          <p>{role}</p>
         </div>
       </div>
     </a>
