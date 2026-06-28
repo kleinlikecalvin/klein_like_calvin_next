2adb9e40ee042809ed786a8bf32c4537666d8035
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-06-28 14:38:38 -0700

    Increases margin top for mobile dropdown menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${merriweather.variable} m-0 mx-auto mt-28 mb-12.5 max-w-300 text-base antialiased lg:mt-40`}
+        className={`${merriweather.variable} m-0 mx-auto mt-36 mb-12.5 max-w-300 text-base antialiased lg:mt-40`}
       >
         <ThemeProvider attribute="class">
           <Nav />
