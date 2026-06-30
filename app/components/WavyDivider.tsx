68a6c0dd9b7d66c33a5072bd156968999ae9a4a7
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-06-29 17:08:13 -0700

    Creates WavyDivider

---
+++ app/components/WavyDivider.tsx
@@ -0,0 +1,18 @@
+export default function WavyDivider() {
+  return (
+    <div className="WavyDivider w-full overflow-hidden">
+      <svg
+        className="text-special-one h-8"
+        viewBox="0 0 220 12"
+        preserveAspectRatio="none"
+        fill="none"
+      >
+        <path
+          d="M0 6 Q5 0 10 6 T20 6 T30 6 T40 6 T50 6 T60 6 T70 6 T80 6 T90 6 T100 6 T110 6 T120 6 T130 6 T140 6 T150 6 T160 6 T170 6 T180 6 T190 6 T200 6 T210 6 T220 6"
+          stroke="currentColor"
+          stroke-width="2"
+        />
+      </svg>
+    </div>
+  );
+}
