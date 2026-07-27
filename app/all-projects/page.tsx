df5f2ee15e429a3c246fc8e3283b6068b0e1a3d4
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-07-27 16:50:29 -0700

    Adds pagination to all-projects page

--- app/all-projects/page.tsx
+++ app/all-projects/page.tsx
@@ -1,22 +1,40 @@
+"use client";
+
+import { useState } from "react";
 import ProjectOverviewCard from "../components/Projects/ProjectOverviewCard";
 import { projectsData } from "../data/projects/projectsData";
-
-// TODO: add pagination so we aren't loading a bunch of projects all at once
+import { Project } from "../types/project_types";
 
 export default function AllProjects() {
+  // Start with displaying only 4 projects
+  const [projects, setProjects] = useState<Project[]>(projectsData.slice(0, 4));
+
+  function getNextFourProjects() {
+    const nextBatch = projectsData.slice(projects.length, projects.length + 4);
+
+    setProjects((prev) => [...prev, ...nextBatch]);
+  }
+
   return (
-    <main className="AllProjects grid grid-cols-1 gap-10 lg:grid-cols-2">
-      {projectsData.map((project, i) => {
-        return (
-          <ProjectOverviewCard
-            project={project}
-            classes="border border-accent-one"
-            key={project.id}
-            position={i}
-          />
-        );
-      })}
-      <button className="load-more">load more</button>
+    <main className="AllProjects">
+      <div className="project-container grid grid-cols-1 gap-10 lg:grid-cols-2">
+        {projects.map((project, i) => {
+          return (
+            <ProjectOverviewCard
+              project={project}
+              classes="border border-accent-one"
+              key={project.id}
+              position={i}
+            />
+          );
+        })}
+      </div>
+      <button
+        className="load-more scale mx-auto my-10"
+        onClick={getNextFourProjects}
+      >
+        load more
+      </button>
     </main>
   );
 }
