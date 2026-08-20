f65d8bfaee972e24ae3c0d7a80fb650cc342d9e8
Nikki Klein <63431649+kleinlikecalvin@users.noreply.github.com>
2026-08-20 08:41:54 -0700

    Updates meta

--- app/data/meta_data.ts
+++ app/data/meta_data.ts
@@ -1,5 +1,5 @@
 export const metaData = {
   title: "Nikki Klein | Front End Software Engineer Portfolio",
   description:
-    "Front end engineer with a track record of championing a large-scale architectural migration, driving technical design decisions, and delivering measurable business impact through performance and UX improvements. Experienced mentoring teammates, championing accessibility, and partnering cross-functionally with product and design. Comfortable owning ambiguity and driving a technical roadmap independently.",
+    "Front end engineer with a track record of championing a large-scale architectural migration, driving technical design decisions, and delivering measurable business impact through performance and UX improvements. Experienced mentoring teammates, championing accessibility, and partnering cross-functionally with product and design. Passionate about languages - spoken and computer.",
 };
